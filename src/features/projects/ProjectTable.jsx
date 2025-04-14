67f3f5739fbb76c86c79998249f72695b344c604
Mehdi Asadi <mehdiasadi63@gmail.com>
2025-04-14 21:23:41 +0330

    add projectRow

--- src/features/projects/ProjectTable.jsx
+++ src/features/projects/ProjectTable.jsx
@@ -1,41 +1,40 @@
 import useOwnerProjects from './useOwnerProjects'
 import Loading from '../../ui/Loading'
 import Empty from '../../ui/Empty'
+import Table from '../../ui/Table'
+import ProjectRow from './ProjectRow'
 
 function ProjectTable() {
 	const { isLoading, projects } = useOwnerProjects()
 
 	if (isLoading) return <Loading />
 
+	if (!projects.length) return <Empty resourceName="پروژه" />
+
 	return (
-		<table>
-			<thead>
-				<tr className="title-row">
-					<th>#</th>
-					<th>عنوان پروژه</th>
-					<th>دسته بندی</th>
-					<th>بودجه</th>
-					<th>ددلاین</th>
-					<th>تگ ها</th>
-					<th>فریلنسر</th>
-					<th>وضعیت</th>
-					<th>عملیات</th>
-					<th>درخواست ها</th>
-				</tr>
-			</thead>
-			<tbody>
+		<Table>
+			<Table.Header>
+				<th>#</th>
+				<th>عنوان پروژه</th>
+				<th>دسته بندی</th>
+				<th>بودجه</th>
+				<th>ددلاین</th>
+				<th>تگ ها</th>
+				<th>فریلنسر</th>
+				<th>وضعیت</th>
+				<th>عملیات</th>
+				<th>درخواست ها</th>
+			</Table.Header>
+			<Table.Body>
 				{projects.map((project, index) => (
-					<tr key={project.id}>
-						<td>{index + 1}</td>
-						<td>{project.title}</td>
-						<td> {project.category.title}</td>
-						<td>{project.budget}</td>
-						<td>{project.deadline}</td>
-					</tr>
+					<ProjectRow
+						key={project._id}
+						project={project}
+						index={index}
+					/>
 				))}
-			</tbody>
-		</table>
+			</Table.Body>
+		</Table>
 	)
 }
-
 export default ProjectTable
--- src/services/projectService.js
+++ src/services/projectService.js
@@ -4,3 +4,6 @@ export function getOwnerProjectsApi() {
 		.get('/project/owner-projects')
 		.then(({ data }) => data.data)
 }
+export function removeProjectApi(id) {
+	return http.delete(`/project/${id}`).then(({ data }) => data.data)
+}
