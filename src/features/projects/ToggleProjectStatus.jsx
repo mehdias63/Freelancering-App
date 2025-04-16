83653f52fe9946ae09e127d991c042c6695f39da
Mehdi Asadi <mehdiasadi63@gmail.com>
2025-04-16 19:18:21 +0330

    add toggle

---
+++ src/features/projects/ToggleProjectStatus.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import useToggleProjectStatus from './useToggleProjectStatus'
+import Loading from '../../ui/Loading'
+import Toggle from '../../ui/Toggle'
+
+function ToggleProjectStatus({ project }) {
+	const { status } = project
+	const { isUpdating, toggleProjectStatus } = useToggleProjectStatus()
+
+	const toggleHandler = () => {
+		const newStatus = status === 'OPEN' ? 'CLOSED' : 'OPEN'
+		toggleProjectStatus({
+			id: project._id,
+			data: { status: newStatus },
+		})
+	}
+
+	return (
+		<div className="w-[5rem]">
+			{isUpdating ? (
+				<Loading height={20} width={50} />
+			) : (
+				<Toggle
+					enabled={status === 'OPEN' ? true : false}
+					label={status === 'OPEN' ? 'باز' : 'بسته'}
+					onChange={toggleHandler}
+				/>
+			)}
+		</div>
+	)
+}
+export default ToggleProjectStatus
