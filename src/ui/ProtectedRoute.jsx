70a3d850d403b022ab0865479ea267c25d498564
Mehdi Asadi <mehdiasadi63@gmail.com>
2025-04-22 21:20:56 +0330

    update app

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -7,11 +7,9 @@ import toast from 'react-hot-toast'
 function ProtectedRoute({ children }) {
 	const navigate = useNavigate()
 
-	// 1. load the authenticated user
 	const { isAuthenticated, isLoading, isAuthorized, isVerified } =
 		useAuthorize()
 
-	// 2. check if is Authorized or not, check is is Authenticagted or not
 	useEffect(() => {
 		if (!isAuthenticated && !isLoading) navigate('/auth')
 		if (!isVerified && !isLoading) {
@@ -22,7 +20,6 @@ function ProtectedRoute({ children }) {
 			navigate('/not-access', { replace: true })
 	}, [isAuthenticated, isAuthorized, isLoading, navigate, isVerified])
 
-	// 3. while loading => show a loading
 	if (isLoading)
 		return (
 			<div className="flex items-center justify-center h-screen bg-secondary-100">
@@ -30,7 +27,6 @@ function ProtectedRoute({ children }) {
 			</div>
 		)
 
-	// 4.  if user isAuthenticated and isAuthrozexd => rendere the app
 	if (isAuthenticated && isAuthorized) return children
 }
 export default ProtectedRoute
